1f5aed633e94c691b1cfc440cbc9cab9b3b5caf2
aquintero <alejandro_quintero@sil.org>
2025-06-25 15:49:36 -0700

    update job.sh

--- job.sh
+++ job.sh
@@ -8,4 +8,4 @@
 #SBATCH --mail-user=alejandro_quintero@sil.org
 #SBATCH --mail-type=ALL
 module load singularity
-singularity exec --env-file .env --nv --bind $PWD/downloaded_datasets:/app/downloaded_datasets --bind /opt/software/NVHPC/25.3-CUDA-12.8.0/Linux_x86_64/25.3/cuda/12.8/compat/libcuda.so.570.124.06:/usr/local/cuda-12.8/targets/x86_64-linux/lib/stubs/libcuda.so vits2_develop.sif bash -c "cd /app && make all"
+singularity exec --env-file .env --nv --bind $PWD/downloaded_datasets:/app/downloaded_datasets --bind /usr/lib64/libcuda.so.575.57.08:/usr/local/cuda-12.4/compat/libcuda.so.550.54.15 vits2_develop.sif bash -c "cd /app && make all"
